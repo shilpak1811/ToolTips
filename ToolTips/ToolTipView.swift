18f4269b5e726d9808b063fff0564d238b55f072
Shilpa Kumari <shilpa.kumari1@go-mmt.com>
2021-01-29 21:27:32 +0530

    Initial commit

---
+++ ToolTips/ToolTipView.swift
@@ -0,0 +1,17 @@
+//
+//  ToolTipView.swift
+//  ToolTips
+//
+//  Created by Shilpa Kumari on 29/01/21.
+//  Copyright © 2021 Shilpa Kumari. All rights reserved.
+//
+
+import UIKit
+
+class ToolTipView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+}
---
+++ ToolTips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToolTips
+//
+//  Created by Shilpa Kumari on 29/01/21.
+//  Copyright © 2021 Shilpa Kumari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
