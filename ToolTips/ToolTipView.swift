0408be9dd3ea0cb3b82cb057908979e6f2d9b063
Shilpa Kumari <shilpa.kumari1@go-mmt.com>
2021-01-29 23:29:31 +0530

    ToolTip configured

--- ToolTips/ToolTipView.swift
+++ ToolTips/ToolTipView.swift
@@ -8,10 +8,71 @@
 
 import UIKit
 
+enum ToolTipPosition {
+    case left
+    case middle
+    case right
+}
+
 class ToolTipView: UIView {
 
-    override func draw(_ rect: CGRect) {
-        
+    var roundRect: CGRect!
+    var toolTipWidth: CGFloat = 20.0
+    var toolTipHeight: CGFloat = 20.0
+    var cornerRadius: CGFloat = 5.0
+    var position: ToolTipPosition = .middle
+    
+    convenience init(frame: CGRect, cornerRadius: CGFloat, position: ToolTipPosition, text: String) {
+        self.init(frame: frame)
+        self.position = position
+        self.cornerRadius = cornerRadius
+        createLabel(text)
     }
     
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        drawToolTip(rect)
+    }
+    
+    private func getTipPath() -> UIBezierPath {
+        let toolTipRect = CGRect(x: roundRect.midX-10, y: roundRect.maxY, width: toolTipWidth, height: toolTipHeight)
+        let trianglePath = UIBezierPath()
+        trianglePath.move(to: CGPoint(x: toolTipRect.minX, y: toolTipRect.minY))
+        trianglePath.addLine(to: CGPoint(x: toolTipRect.maxX, y: toolTipRect.minY))
+        trianglePath.addLine(to: CGPoint(x: toolTipRect.midX, y: toolTipRect.maxY))
+        trianglePath.addLine(to: CGPoint(x: toolTipRect.minX, y: toolTipRect.minY))
+        trianglePath.close()
+        return trianglePath
+    }
+    
+    private func drawToolTip(_ rect: CGRect) {
+        roundRect = CGRect(x: rect.minX, y: rect.minY, width: rect.width, height: rect.height-toolTipHeight)
+        let roundRectBez = UIBezierPath(roundedRect: roundRect, cornerRadius: cornerRadius)
+        let triangle = getTipPath()
+        roundRectBez.append(triangle)
+        let shape = getShapeLayer(roundRectBez.cgPath)
+        self.layer.insertSublayer(shape, at: 0)
+    }
+    
+    private func getShapeLayer(_ path: CGPath) -> CAShapeLayer {
+        let shape = CAShapeLayer()
+        shape.path = path
+        shape.fillColor = UIColor.black.cgColor
+        
+        shape.shadowColor = UIColor.black.withAlphaComponent(0.6).cgColor
+        shape.shadowOffset = CGSize(width: 0, height: 2)
+        shape.shadowRadius = cornerRadius
+        shape.shadowOpacity = 0.8
+        return shape
+    }
+    
+    private func createLabel(_ text : String){
+        let label = UILabel(frame: CGRect(x: 8, y: 5, width: frame.width-16, height: frame.height-toolTipHeight-10))
+        label.text = text
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        addSubview(label)
+    }
 }
--- ToolTips/ViewController.swift
+++ ToolTips/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var isToolTipShown: Bool = false
+    private var toolTip: ToolTipView!
+    @IBOutlet weak var showTootipButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showTooltipTapped(_ sender: UIButton) {
+        isToolTipShown ? removeToolTip() : showToolTip()
+        isToolTipShown = !isToolTipShown
+    }
+    
+    private func showToolTip() {
+        let tooltipViewWidth: CGFloat = 200
+        let tooltipViewHeight: CGFloat = 200
+        let rect = CGRect(x: showTootipButton.frame.midX-tooltipViewWidth/2, y: showTootipButton.frame.minY-tooltipViewHeight, width: tooltipViewWidth, height: tooltipViewHeight)
+        toolTip = ToolTipView(frame: rect, cornerRadius: 10.0, position: .middle, text: "Hello! Please feel free to fill the useful information for the user here")
+        UIApplication.shared.keyWindow?.addSubview(toolTip)
+        performShow(toolTip)
+    }
+    
+    private func performShow(_ v: UIView?) {
+        v?.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        UIView.animate(withDuration: 0.3, delay: 0.05, options: .curveEaseOut, animations: {
+            v?.transform = .identity
+        }) { finished in
+        }
+    }
+    
+    private func removeToolTip() {
+        performHide(toolTip)
+    }
+    
+    private func performHide(_ v: UIView?) {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+            v?.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        }) { finished in
+            self.toolTip.removeFromSuperview()
+        }
+    }
 }
 
